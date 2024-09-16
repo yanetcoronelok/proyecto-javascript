488af7f257552e2bee3522fa3baddf2bb390aa1e
yanetcoronelok <ya.coronel@gmail.com>
2024-09-16 19:57:23 -0300

    Clase 1

---
+++ clase-01/js/index.js
@@ -0,0 +1,56 @@
+// como se declara var: 
+var numero = 18;
+
+// como se declara let:
+let veinteYUno = 21;
+console.log(veinteYUno);
+
+// como se declara const:
+const VEINTE_Y_DOS = 22;
+
+//declarar
+let edad;
+let primerNombre;
+let apellido;
+
+//asignacion
+edad = 18;
+primerNombre = "Meredith";
+apellido = "Grey";
+
+
+veinteYUno = "veinte y uno";
+console.log(veinteYUno);
+
+
+console.log(VEINTE_Y_DOS);
+
+// VEINTE_Y_DOS = "VEINTE Y DOS";
+// console.log(VEINTE_Y_DOS);
+
+// operadores
+
+//let numeroUno = 8;
+//let numeroDos = 10;
+
+// let suma = numeroUno + numeroDos;
+//console.log(suma);
+
+
+//let suma = numeroUno + numeroDos;
+//console.log("El resultado de la suma es " + suma);
+
+
+//let palabraUno = "Cinco";
+//let palabraDos = "Seis";
+
+//let palabras = palabraUno + " " + palabraDos;
+//console.log(palabras);
+
+let numeroUno = parseInt(prompt("Por favor ingresa un numero"));
+let numeroDos = parseInt(prompt("Ingresa otro numero"));
+let suma = numeroUno + numeroDos;
+
+
+console.log(suma);
+
