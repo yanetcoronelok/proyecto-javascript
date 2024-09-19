d8ce19454817e2e75c816c17f072a3aef467286a
yanetcoronelok <ya.coronel@gmail.com>
2024-09-18 21:01:22 -0300

    Ejercicios de logica

---
+++ ejercicios-logica/js/index.js
@@ -0,0 +1,38 @@
+/*1-Realizá un programa que permita ingresar 3 notas pertenecientes  para cierto alumno de nivel secundario. Debe calcularse y mostrarse la nota promedio. [EC]  */
+
+/* 
+let nota1;
+let nota2;
+let nota3;
+let promedio;
+
+nota1 = parseInt(prompt("Ingresar primer nota del trimestre:"));
+nota2 = parseInt(prompt("Ingresar segunda nota del trimestre:"));
+nota3 = parseInt(prompt("Ingresar tercer nota del trimestre:"));
+
+promedio = (nota1 + nota2 + nota3)/3;
+
+    console.log("La nota promedio es " +  promedio.toFixed(2));
+
+    */
+
+
+    /* 2-Realizá un programa que permita ingresar un número entero. 
+    Debe mostrarse el número ingresado: a. Multiplicado por 5. b. Dividido por 2. */
+
+let numeroEntero = parseInt(prompt("Ingresa tu numero: "));
+
+console.log(numeroEntero);
+
+let multiplicadoPorCinco;
+let divididoPorDos;
+
+multiplicadoPorCinco = numeroEntero*5;
+console.log(multiplicadoPorCinco);
+
+
+divididoPorDos = multiplicadoPorCinco/2;
+console.log(divididoPorDos.toFixed(2));
+
+
+
